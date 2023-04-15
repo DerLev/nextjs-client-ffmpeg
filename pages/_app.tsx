068e1b27851acd1dafc984e32f91caa66c222fc7
DerLev <levin.schroeren@gmail.com>
2023-04-15 16:08:50 +0200

    adding mantine.dev

---
+++ pages/_app.tsx
@@ -0,0 +1,29 @@
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+import { MantineProvider } from '@mantine/core'
+import { RouterTransition } from '@/components/RouterTransition'
+
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props
+
+  return (
+    <>
+      <Head>
+        <title>ffmpeg Client Converter</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: 'dark',
+        }}
+      >
+        <RouterTransition />
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  )
+}
---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next'
+import { Text } from '@mantine/core'
+
+const Index: NextPage = () => (
+  <Text>This is Mantine</Text>
+)
+
+export default Index
